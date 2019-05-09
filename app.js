f475d441f2d6d13fdd9377a369451904a1db87ea
lexyking <sahiealexandre@hotmail.fr>
2019-05-09 18:51:14 +0100

    Joan design js and html modif

--- app.js
+++ app.js
@@ -1,27 +1,50 @@
-document.addEventListener("keydown", (e)=>{
-// console.log(e.keyCode);
-const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-const key = document.querySelector(`.boxNote[data-key="${e.keyCode}"]`);
+const main = document.querySelector('main');
+
+console.log(main)
+// main.addEventListener('keydown', (e) => {
+// 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+// 	const key = document.querySelector(`.boxNote[data-key="${e.keyCode}"]`);
+// 	if (!audio) return;
+
+// 	audio.currentTime = 0;
+
+// 	audio.play();
+
+// 	key.classList.add('playing');
+
+// 	console.log(e.keyCode)
+// })
 
 
-if(!audio) return;
+// document.addEventListener('keydown', e => {
+// 	// console.log(e.keyCode);
+// 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+// 	const key = document.querySelector(`.boxNote[data-key="${e.keyCode}"]`);
 
-audio.currentTime=0;
+// 	if (!audio) return;
 
-audio.play();
+// 	audio.currentTime = 0;
 
-key.classList.add("playing");
-});
+// 	audio.play();
 
-
-function removeTransition(val){
-	if(val.propertyName !=="transform") return;
-	this.classList.remove("playing");
-	console.log(val.propertyName);
-}
-const endAnimation = document.querySelectorAll(".boxNote");
-endAnimation.forEach(element => element.addEventListener("transitionend", removeTransition));
-
-// document.addEventListener("click", (e)=>{
-// console.log(e.keyCode);
+// 	key.classList.add('playing');
 // });
+
+function removeTransition(val) {
+	if (val.propertyName !== 'transform') return;
+	this.classList.remove('playing');
+	// console.log(val.propertyName);
+}
+const boxNotes = document.querySelectorAll('.boxNote');
+boxNotes.forEach(element => element.addEventListener('transitionend', removeTransition));
+
+
+
+boxNotes.forEach(boxNote => boxNote.addEventListener('click',
+		() => {
+			const audio = document.querySelector(`audio[data-key='${boxNote.getAttribute('data-key')}'`);
+			audio.play();
+			boxNote.classList.add('playing');
+		})
+
+);
